6125f0e4d2e67f6f6f780cf247f54579af48c3f6
souta-web <souta200319@gmail.com>
2022-10-19 15:11:39 +0900

    aa

---
+++ パストーン/data/scenario/ゲームとは関係ない/タグ除去/いろいろ試す.py
@@ -0,0 +1,6 @@
+str_list = ["abcdefg","bbb","ccc"]
+a_list = [["00","01"]]
+
+
+for i in a_list:
+    print(i[1])
--- パストーン/data/scenario/ゲームとは関係ない/タグ除去/タグ除去.py
+++ パストーン/data/scenario/ゲームとは関係ない/タグ除去/タグ除去.py
@@ -1,27 +1,73 @@
+import re
+
 class delete_program:
     def __init__(self):
-        self.inputFile = "input.txt"     # 読み込みファイル
-        self.outputFile = "output.txt"   # 書き込みファイル
-
-    def init_delete_line(self):
-    #１回の実行で１回しか実行されない
+        pass
+    
+    #文字修正を適用するテキストファイル読み込み
+    def delete_program_init(self):
         
-        for line in open(self.inputFile):
-            print(line)
-        
-        
-        #削除する起点となる文字
-        delete_symbol_list = [";"]
-        symbol_volume = len(delete_symbol_list)
+        #全文読み込み
+        with open('タグ除去\input.txt', 'r' ,encoding="utf-8") as f:
+            input_list = f.read().split("\n")
 
+        #読み込んだものを１行ずつリストに入れる
+        self.text_lines_list = [] #行数分の要素が入る
+        count = 0
+        for line in input_list:
+            self.text_lines_list.append(line)
+            #print(self.text_lines_list[count])
+            count += 1
 
-    #特定の行を削除するプログラム
-    def delete_line(self):
-        
-        for line in open(self.full_txt):
-            print(line)
+    #文字列の先頭が特定の文字の時にその行を削除する
+    def delete_symbol_headofline(self):
+        count = 0 #削除対象の行数カウント
+        symbol_headofline_list = [";","@"]
+        for line in self.text_lines_list:
+            for j in symbol_headofline_list:
+                if len(line) == 0:
+                    pass
 
-    #delete_line()
+                elif line[0] == j:
+                    self.text_lines_list[count] = ""
+            
+            #print(self.text_lines_list[count])
+            count += 1
+
+    def deltete_symbol_sandwich(self):
+        count = 0 #削除対象の行数カウント
+        delete = "false"
+        delete_start = 0
+        delete_end = 0
+        symbol_sandwich_list = [["[","]"]]
+        for j in symbol_sandwich_list:
+            for line in self.text_lines_list:
+                delete_start = 0
+                delete_end = 0
+                for k in range(len(line)):
+                    #文字が削除対象の文字と一致していたら
+                    if line[k] == j[0]:
+                        #print(j[0])
+                        delete = "true"
+                        delete_start = k #行の先頭から数えたときの削除開始位置
+                    elif line[k] == j[1]:
+                        #print(j[1])
+                        delete = "true"
+                        delete_end = k #終了位置
+                    else:
+                        pass
+                    
+                    #文字消す
+                    if delete == "true":
+                        self.text_lines_list[count].replace(str(self.text_lines_list[count])[delete_start:delete_end]," ")
+                        delete = "false"
+
+                print(self.text_lines_list[count])
+                count += 1
+                
 
 delete_program = delete_program()
-delete_program.init_delete_line()
+#delete_program.init_delete_line()
+delete_program.delete_program_init()
+delete_program.delete_symbol_headofline()
+delete_program.deltete_symbol_sandwich()
