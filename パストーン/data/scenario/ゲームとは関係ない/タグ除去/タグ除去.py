5a9b05ed512791a9db85425c94acc71a95b8399c
souta-web <souta200319@gmail.com>
2022-10-21 23:29:24 +0900

    タグ除去

--- パストーン/data/scenario/ゲームとは関係ない/タグ除去/タグ除去.py
+++ パストーン/data/scenario/ゲームとは関係ない/タグ除去/タグ除去.py
@@ -1,3 +1,5 @@
+import enum
+from itertools import count
 import re
 
 class delete_program:
@@ -36,38 +38,90 @@ class delete_program:
 
     def deltete_symbol_sandwich(self):
         count = 0 #削除対象の行数カウント
-        delete = "false"
+        #delete = "false"
         delete_start = 0
         delete_end = 0
         symbol_sandwich_list = [["[","]"]]
+        delete_st = "false"
+        delete_et = "false"
+        
         for j in symbol_sandwich_list:
+            count = 0
             for line in self.text_lines_list:
                 delete_start = 0
                 delete_end = 0
                 for k in range(len(line)):
                     #文字が削除対象の文字と一致していたら
-                    if line[k] == j[0]:
-                        #print(j[0])
-                        delete = "true"
+                    if str(line[k]) == j[0]:
+                        #print(str(j[0]) + "j[0]です")
+                        delete_st = "true"
                         delete_start = k #行の先頭から数えたときの削除開始位置
-                    elif line[k] == j[1]:
-                        #print(j[1])
-                        delete = "true"
+                        #print(delete_start)
+                    elif str(line[k]) == j[1]:
+                        #print(str(j[1]) + "j[1]です")
+                        delete_et = "true"
                         delete_end = k #終了位置
+                        #print(delete_end)
                     else:
                         pass
                     
                     #文字消す
-                    if delete == "true":
-                        self.text_lines_list[count].replace(str(self.text_lines_list[count])[delete_start:delete_end]," ")
-                        delete = "false"
+                    if delete_st == "true" and delete_et == "true":
+                        #print("deleteしたよ")
+                        #print(type(self.text_lines_list[count]))
+                        self.text_lines_list[count]=str(self.text_lines_list[count]).replace(str(self.text_lines_list[count])[delete_start:delete_end+1],"")
+                        delete_st = "false"
+                        delete_et = "false"
 
-                print(self.text_lines_list[count])
+                #print(self.text_lines_list[count])
                 count += 1
-                
+
+
+    def output_txt(self):
+        previous_newline="false"
+        print("書き出しだよ")
+        f = open('タグ除去\output.txt', 'w',encoding="utf-8")
+        for i in self.text_lines_list:
+            if i=="" and previous_newline=="false":
+                #print(0)
+                previous_newline = "true"
+                f.write(i + "\n")
+            elif i=="":
+                #print(1)
+                pass
+            elif i!="":
+                #print(2)
+                f.write(i + "\n")
+                previous_newline = "false"
+
+        f.close()
+
+    """def delete_newline(self):
+        count_newline=0
+        continue_newline = "false"
+        count_delete = 0
+        for i,j in enumerate(self.text_lines_list):
+            if j == "":
+                count_newline += 1
+                continue_newline = "true"
+            
+            else:
+                if continue_newline=="true" and count_newline>=2:
+                    for k in range(count_newline-1):
+                        del self.text_lines_list[i-count_newline-count_delete+k]
+                        print("改行削除")
+
+                    count_delete += count_newline
+                    count_newline = 0"""
+                    
+
 
 delete_program = delete_program()
 #delete_program.init_delete_line()
 delete_program.delete_program_init()
 delete_program.delete_symbol_headofline()
 delete_program.deltete_symbol_sandwich()
+
+#delete_program.delete_newline()
+
+delete_program.output_txt()
